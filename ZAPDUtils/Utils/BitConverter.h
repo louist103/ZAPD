651a88847170b0a12be4d74d49f3824104041433
Louis <35883445+louist103@users.noreply.github.com>
2021-08-27 22:34:55 -0400

    heeeeelp

--- ZAPDUtils/Utils/BitConverter.h
+++ ZAPDUtils/Utils/BitConverter.h
@@ -4,26 +4,27 @@
 #include <stdint.h>
 #include <vector>
 
-namespace BitConverter
+class BitConverter
 {
-static int8_t ToInt8BE(const uint8_t* data, int32_t offset);
-static int8_t ToInt8BE(const std::vector<uint8_t>& data, int32_t offset);
-static uint8_t ToUInt8BE(const uint8_t* data, int32_t offset);
-static uint8_t ToUInt8BE(const std::vector<uint8_t>& data, int32_t offset);
-static int16_t ToInt16BE(const uint8_t* data, int32_t offset);
-static int16_t ToInt16BE(const std::vector<uint8_t>& data, int32_t offset);
-static uint16_t ToUInt16BE(const uint8_t* data, int32_t offset);
-static uint16_t ToUInt16BE(const std::vector<uint8_t>& data, int32_t offset);
-static int32_t ToInt32BE(const uint8_t* data, int32_t offset);
-static int32_t ToInt32BE(const std::vector<uint8_t>& data, int32_t offset);
-static uint32_t ToUInt32BE(const uint8_t* data, int32_t offset);
-static uint32_t ToUInt32BE(const std::vector<uint8_t>& data, int32_t offset);
-static int64_t ToInt64BE(const uint8_t* data, int32_t offset);
-static int64_t ToInt64BE(const std::vector<uint8_t>& data, int32_t offset);
-static uint64_t ToUInt64BE(const uint8_t* data, int32_t offset);
-static uint64_t ToUInt64BE(const std::vector<uint8_t>& data, int32_t offset);
-static float ToFloatBE(const uint8_t* data, int32_t offset);
-static float ToFloatBE(const std::vector<uint8_t>& data, int32_t offset);
-static double ToDoubleBE(const uint8_t* data, int32_t offset);
-static double ToDoubleBE(const std::vector<uint8_t>& data, int32_t offset);
+public:
+	static int8_t ToInt8BE(const uint8_t* data, int32_t offset);
+	static int8_t ToInt8BE(const std::vector<uint8_t>& data, int32_t offset);
+	static uint8_t ToUInt8BE(const uint8_t* data, int32_t offset);
+	static uint8_t ToUInt8BE(const std::vector<uint8_t>& data, int32_t offset);
+	static int16_t ToInt16BE(const uint8_t* data, int32_t offset);
+	static int16_t ToInt16BE(const std::vector<uint8_t>& data, int32_t offset);
+	static uint16_t ToUInt16BE(const uint8_t* data, int32_t offset);
+	static uint16_t ToUInt16BE(const std::vector<uint8_t>& data, int32_t offset);
+	static int32_t ToInt32BE(const uint8_t* data, int32_t offset);
+	static int32_t ToInt32BE(const std::vector<uint8_t>& data, int32_t offset);
+	static uint32_t ToUInt32BE(const uint8_t* data, int32_t offset);
+	static uint32_t ToUInt32BE(const std::vector<uint8_t>& data, int32_t offset);
+	static int64_t ToInt64BE(const uint8_t* data, int32_t offset);
+	static int64_t ToInt64BE(const std::vector<uint8_t>& data, int32_t offset);
+	static uint64_t ToUInt64BE(const uint8_t* data, int32_t offset);
+	static uint64_t ToUInt64BE(const std::vector<uint8_t>& data, int32_t offset);
+	static float ToFloatBE(const uint8_t* data, int32_t offset);
+	static float ToFloatBE(const std::vector<uint8_t>& data, int32_t offset);
+	static double ToDoubleBE(const uint8_t* data, int32_t offset);
+	static double ToDoubleBE(const std::vector<uint8_t>& data, int32_t offset);
 };  // namespace BitConverter
